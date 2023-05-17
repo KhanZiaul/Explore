8c995e19cec23ab62f726ce8ae050428d091c16c
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-05-17 15:34:55 +0600

    added MongoDB Driver

--- Explore Server/index.js
+++ Explore Server/index.js
@@ -1,12 +1,43 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const app = express()
 const port = process.env.PORT || 1000
 
 // Middleware
 app.use(cors())
 app.use(express.json())
+
+
+// MongoDB
+
+const uri = `mongodb+srv://${process.env.DB_TITLE}:${process.env.DB_PASS}@cluster0.mf37tl1.mongodb.net/?retryWrites=true&w=majority`
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 
 app.get('/', (req,res) => {

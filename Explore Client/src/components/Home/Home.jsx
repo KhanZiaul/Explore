650e7a38888dbe6cc31c28cbf3ffddbf06eada2e
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-05-17 17:16:53 +0600

    added Footer

--- Explore Client/src/components/Home/Home.jsx
+++ Explore Client/src/components/Home/Home.jsx
@@ -1,9 +1,13 @@
+import { Outlet } from 'react-router-dom';
 import Nav from '../Shared/Nav/Nav';
+import Footer from '../Shared/Footer/Footer';
 
 const Home = () => {
     return (
         <div className='p-5'>
             <Nav></Nav>
+            <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };

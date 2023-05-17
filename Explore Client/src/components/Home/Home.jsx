46c55d851632a62b9abd179fe823a405c3663868
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-05-17 17:56:25 +0600

    cards added

--- Explore Client/src/components/Home/Home.jsx
+++ Explore Client/src/components/Home/Home.jsx
@@ -1,10 +1,17 @@
+import { useLoaderData } from "react-router-dom";
 import Jobs from "../Jobs/Jobs";
 
 
 const Home = () => {
+    const jobs = useLoaderData()
+    console.log(jobs)
     return (
         <div >
-            <Jobs></Jobs>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 my-10">
+                {
+                    jobs?.map(job => <Jobs key={job._id} job={job}></Jobs>)
+                }
+            </div>
         </div>
     );
 };
--- Explore Client/src/components/Jobs/Jobs.jsx
+++ Explore Client/src/components/Jobs/Jobs.jsx
@@ -1,9 +1,21 @@
 
 
-const Jobs = () => {
+const Jobs = ({ job }) => {
+    const { employment_type, image, job_category, job_title, location, salary, vacancy, _id } = job
     return (
         <div>
-            
+            <div className="card w-96 bg-base-100 shadow-xl">
+                <figure><img className="h-96 w-96" src={image} alt="jobs"  /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">Shoes!</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+
+
         </div>
     );
 };
